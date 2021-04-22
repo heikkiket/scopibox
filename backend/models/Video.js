05f5f06eae93c04e11902927bb0b12754eb0033f
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-04-22 21:35:30 +0300

    Removed useless code

--- backend/models/Video.js
+++ backend/models/Video.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({ title: String, url: String });
 
-schema.statics.findRandom = async function () {
-  return await this.find({}, null, {limit: 1});
-};
-
 const Video = mongoose.model("Video", schema);
 
 export default Video;
