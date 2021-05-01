61f54501be19f6660098a19ee363370cf98f8f1f
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-05-01 17:36:40 +0300

    Refactored schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,15 @@
 import loadSchema from "./loadSchema.js";
+import { gql } from "apollo-server"
 
+const rootSchema = gql`
+type Query {
+_empty: String
+}
+type Mutation {
+_empty: String
+}
+`;
 const videoSchema = loadSchema("video.graphql");
+const userSchema = loadSchema("user.graphql");
 
-export default [videoSchema];
+export default [rootSchema, videoSchema, userSchema];
