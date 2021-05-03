29976050a370f4142b066574371199fe77cced62
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-05-03 22:17:05 +0300

    A simple router guard for unauthenticated user

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import Home from "../views/Home.vue";
 import History from "../views/History.vue";
 import Login from "../views/Login.vue";
+import store from "../store";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+  },
   {
     path: "/home",
     name: "Home",
@@ -17,15 +24,19 @@ const routes = [
     name: "History",
     component: History,
   },
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-  }
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // Login is the only unauthenthicated route for now
+  if (to.name === "Login") next();
+
+  if (!store.state.loggedin) next({ name: "Login" });
+
+  next();
 });
 
 export default router;
