aafb4aa109d997c7a722de7d18f33b5257f323ef
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-05-03 20:01:53 +0300

    Missing files

---
+++ frontend/src/views/History.vue
@@ -0,0 +1,14 @@
+<template>
+  <History id="history" />
+</template>
+
+<script>
+import History from "../components/History";
+
+export default {
+  name: "HistoryView",
+  components: {
+    History,
+  },
+};
+</script>
