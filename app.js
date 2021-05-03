2b010f59b04947201b159da5e5c4cc9f3d284d78
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2021-05-03 17:49:55 +0300

    Added optional https redirection

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ const app = express();
 
 app.use("/", express.static("public"));
 
+if (process.env.REDIRECT_HTTP) {
+  app.use(function (request, response) {
+    if (!request.secure) {
+      response.redirect("https://" + request.headers.host + request.url);
+    }
+  });
+}
+
 export default app;
